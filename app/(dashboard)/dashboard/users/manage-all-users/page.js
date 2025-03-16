39a6481565e0575a4ab10bf0f36a9073a7a94dfb
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2025-03-16 10:45:19 +0600

    manage users design complete

--- app/(dashboard)/dashboard/users/manage-all-users/page.js
+++ app/(dashboard)/dashboard/users/manage-all-users/page.js
@@ -1,10 +1,18 @@
+import { DataTable } from '@/components/ReusableTable/data-table'
+import { manageUsersColumns } from '@/components/ReusableTable/manageUsersColumn'
+import { payments } from '@/data/payments'
 import React from 'react'
 
 const ManageAllUsersPage = () => {
   return (
-    <div>
-      this is manage users page
-    </div>
+    <div className="container mx-auto py-10">
+          <DataTable
+            columns={manageUsersColumns}
+            data={payments}
+            filterInputPlaceholder={"Search Pages by Title"}
+            filterInputColumn={"title"}
+          />
+        </div>
   )
 }
 
