9f8b9378bb57f62088412bf6a1d48edb24c3d360
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2025-03-16 12:29:23 +0600

    technically create plan design complete

---
+++ app/(dashboard)/dashboard/plans/create-plan/page.js
@@ -0,0 +1,12 @@
+import CreatePlanForm from '@/components/create-plan-form'
+import React from 'react'
+
+const CreatePlan = () => {
+  return (
+    <div>
+      <CreatePlanForm/>
+    </div>
+  )
+}
+
+export default CreatePlan
---
+++ components/create-plan-form.jsx
@@ -0,0 +1,276 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Textarea } from "./ui/textarea";
+import MDEditor from "@uiw/react-md-editor";
+import { useState } from "react";
+import { Separator } from "./ui/separator";
+import { categoryFormSchema } from "@/schemas/category-form-schema";
+import { Switch } from "./ui/switch";
+
+const CreatePlanForm = () => {
+  const form = useForm({
+    resolver: zodResolver(categoryFormSchema),
+    defaultValues: {
+      name: "",
+      slug: "",
+      title: "",
+      description: "",
+      metaTitle: "",
+      metaDescription: "",
+      parentCategory: "",
+    },
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        {/* form first section design */}
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+          {/* create new form */}
+
+          <div className="space-y-6">
+            <h2 className="text-2xl font-semibold">Create New Plan</h2>
+            {/* name */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter Plan Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* description */}
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Enter Plan Description" {...field} />
+                  </FormControl>
+                  <FormDescription>Plan Description</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Prices */}
+            <div className="grid grid-cols-2 gap-4">
+              {/* Yearly Price */}
+              <FormField
+                control={form.control}
+                name="yearlyPrice"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Yearly Price</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={"max-w-sm"}
+                        placeholder="Enter Yearly Price"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Monthly Price */}
+              <FormField
+                control={form.control}
+                name="monthlyPrice"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Monthly Price</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={"max-w-sm"}
+                        placeholder="Enter Monthly Price"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Features */}
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="allow_api"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col  rounded-lg border p-3 shadow-sm max-w-sm">
+                    <div className="space-y-0.5">
+                      <FormLabel>Allow Api</FormLabel>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="no_ads"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col  rounded-lg border p-3 shadow-sm max-w-sm">
+                    <div className="space-y-0.5">
+                      <FormLabel>No Ads</FormLabel>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+
+             <Separator className={"xl:hidden block my-4"} />   
+          {/* options form */}
+
+          <div className="space-y-6">
+            <h2 className="text-2xl font-semibold">Options</h2>
+            {/* options form fields */}
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4" >
+              {/* daily usage */}
+              <FormField
+                control={form.control}
+                name="dailyUsege"
+                render={({ field }) => (
+                  <FormItem className={"max-w-sm"}>
+                    <FormLabel>Daily Usage</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter Daily Usage" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Daily usage restrictions refer to the limitations placed
+                      on the number of requests that a user can perform within a
+                      24-hour period.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* word count */}
+              <FormField
+                control={form.control}
+                name="wordCount"
+                render={({ field }) => (
+                  <FormItem className={"max-w-sm"}>
+                    <FormLabel>Word Count</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter Word Count" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Word count limitation refers to the maximum number of
+                      words allowed in a text, such as in a article rewrite or
+                      form field.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* File Size */}
+              <FormField
+                control={form.control}
+                name="fileSize"
+                render={({ field }) => (
+                  <FormItem className={"max-w-sm"}>
+                    <FormLabel>File Size</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter File Size" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      File size limitation refers to the maximum size of a file
+                      that can be uploaded in tools, the size is in megabytes.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Number Of Image */}
+              <FormField
+                control={form.control}
+                name="numberOfImage"
+                render={({ field }) => (
+                  <FormItem className={"max-w-sm"}>
+                    <FormLabel>Number Of Image</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter Number Of Image" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Image limitation refers to the maximum number of images
+                      that can be uploaded on supported tools.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Number Of Domain */}
+              <FormField
+                control={form.control}
+                name="numberOfDomain"
+                render={({ field }) => (
+                  <FormItem className={"max-w-sm"}>
+                    <FormLabel>Number Of Domain</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter Number Of Domain" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Domain limitation refers to the maximum number of domains
+                      that can be processed in single request.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+        </div>
+
+        <Button type="submit">Save</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default CreatePlanForm;
